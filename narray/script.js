bdf103accc5db7b181f0f4ca756c957ef3c861b6
eldin001 <eldin.balje@icloud.com>
2024-03-14 12:13:41 +0100

    primo

---
+++ narray/script.js
@@ -0,0 +1,9 @@
+/* Fai inserire un numero, che chiameremo N, all'utente.
+    Genera con una funzione N array, ognuno formato da 10 numeri casuali da 1 a 100.
+    Ogni volta che ne crei uno, stampalo. */
+
+    let numero = parseInt(prompt('Inserisci un numero'));
+
+    function creaArray () {
+        let arrayCasuali = numero
+    }
--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
 // Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
-let conatainer = [];
+const numbers = [];
 
-let userNumber = parseInt(prompt('Inserisci un numero'));
+let somma = 0;
 
-while (conatainer < 50) {
-    conatainer.push(userNumber);
-    console.log(conatainer);
-    userNumber = parseInt(prompt('Inserisci un numero'));
-    
+while (somma < 50) {
+    const newNumber = parseInt(prompt('Inserisci un numero;'));
+    if (!isNaN(newNumber)) {
+        somma += newNumber;
+        numbers.push(newNumber);
+    }
 }
+
 
 
 
